fb6b4b7fb39976e1f9be09b0c11b10319689e7ba
Simon Kleber <kleber.simon@t-online.de>
2024-04-21 20:13:09 +0200

    Add initial infrastructure code

---
+++ infrastructure/src/app.ts
@@ -0,0 +1,9 @@
+#!/usr/src/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { ReactAppStack } from './react-app-stack';
+
+const app = new cdk.App();
+
+new ReactAppStack(app, 'InfrastructureStack', {
+});
---
+++ infrastructure/src/react-app-stack.ts
@@ -0,0 +1,11 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class ReactAppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'ReactAppDeploymentBucket')
+  }
+}
