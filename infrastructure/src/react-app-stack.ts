057bc271b35807274ada85271d0fa4ed85a31ea1
Simon Kleber <kleber.simon@t-online.de>
2024-05-01 20:26:36 +0200

    Add deployment and cloudfront distribution

--- infrastructure/src/react-app-stack.ts
+++ infrastructure/src/react-app-stack.ts
@@ -1,11 +1,47 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as s3_deploy from 'aws-cdk-lib/aws-s3-deployment'
+import * as cf from 'aws-cdk-lib/aws-cloudfront'
+import * as cf_origin from 'aws-cdk-lib/aws-cloudfront-origins'
 
 export class ReactAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    new s3.Bucket(this, 'ReactAppDeploymentBucket')
+    const deploymentBucket = new s3.Bucket(this, 'ReactAppDeploymentBucket')
+    new s3_deploy.BucketDeployment(this, 'ReactAppDeployment', {
+      sources: [s3_deploy.Source.asset('../frontend/dist')],
+      destinationBucket: deploymentBucket
+    })
+
+    const originAccessIdentity = new cf.OriginAccessIdentity(
+      this,
+      'ReactAppOriginAccessIdentity'
+    )
+    deploymentBucket.grantRead(originAccessIdentity)
+
+    const distribution = new cf.Distribution(this, 'ReactAppDistribution', {
+      defaultBehavior: { origin: new cf_origin.S3Origin(deploymentBucket) },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        // Single Page App takes care of routing. Serve every path with index.html
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html'
+        },
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html'
+        }
+      ]
+    })
+
+    // output the CloudFront domain name
+    new cdk.CfnOutput(this, 'ReactAppDistributionDomainName', {
+      value: distribution.distributionDomainName
+    })
   }
 }
